a3b863e23480977754dd6f3784290eb15d4c63e4
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-07-03 11:33:32 +0200

    added TestApp from webflux-starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.0.RELEASE"
+	id("org.springframework.boot") version "2.3.1.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation("no.skatteetaten.aurora.springboot:aurora-spring-boot-webflux-starter:1.0.2")
+    implementation("no.skatteetaten.aurora.springboot:aurora-spring-boot-webflux-starter:1.0-SNAPSHOT")
     implementation("io.github.microutils:kotlin-logging:1.7.8")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
